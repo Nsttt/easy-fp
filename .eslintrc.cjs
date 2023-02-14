783593c267cf3a2dfd44215eba98ad7497ab9e5a
Nestor <elnestinatorHD@gmail.com>
2023-02-14 22:00:10 +0100

    Init

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: "./",
+    project: [
+      "./tsconfig.eslint.json",
+      "./examples/*/tsconfig.json",
+      "./packages/*/tsconfig.json",
+    ],
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+  },
+};
