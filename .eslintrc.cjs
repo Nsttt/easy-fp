bc04ac6540bc8c8a91bc2cb7efd8857251de7252
Nestor <elnestinatorHD@gmail.com>
2023-02-20 19:40:39 +0100

    Tweak main eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
+  reportUnusedDisableDirectives: true,
   parser: "@typescript-eslint/parser",
   parserOptions: {
     tsconfigRootDir: "./",
